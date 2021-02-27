3c887c6ac1e3fc22c548b15eb62b321aed56fcf4
Noel Injaga <injaganoel@gmail.com>
2021-02-27 14:37:21 +0300

    to add a full html before editing and stling

---
+++ JS/Script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$ ("contact-detail").submit(function(event){
+  event.preventdefault();
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+  var name=document.getElementById("name").value;
+  var message=document.getElementById("msg").value;
+});
+
+});
