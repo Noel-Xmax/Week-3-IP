e152314420cd0918d1f22a6ffe245586e1f5bbad
Noel Injaga <injaganoel@gmail.com>
2021-02-28 23:29:15 +0300

    To add a README file and update on the formatting

--- JS/Script.js
+++ JS/Script.js
@@ -11,9 +11,36 @@ $ ('.submit').click(function(event){
     alert("Hello" + name +" have received your message. Thank you fro reaching out to us");
   }
 });
+$('#desig').click(function(){
+  $('#view1').toggle();
+});
+$('#dev').click(function(){
+  $('#view2').toggle();
+});
+$('#prod').click(function(){
+  $('#view3').toggle();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
-// $('.design').click(function(){
+// $(".design").click(function(){
+// $("#view1").toggle();
+// });
+
+// });
+// // $('.design').click(function(){
 //   $("#design").toggle();
 //   $(".hide").toggle();
 // })
@@ -25,21 +52,30 @@ $ ('.submit').click(function(event){
 //   $("#product").toggle();
 //   $(".hide").toggle();
 // })
-$('#Image1').click(function(){
-  $('.design').toggle(1000);
-  $('#view1').toggle(1000);
-});
-$('#Image2').click(function(){
-  $('.development').toggle(1000);
-  $('#view2').toggle(1000);
-});
-$('#Image3').click(function(){
-  $('.product').toggle(1000);
-  $('#view3').toggle(1000);
-});
-$('#portfolio').hover(function(){
-    $(this).animate({opacity:"1"});
-    function:(this){
-    $(this) animate({opacity:"0"});
-  });
-});
+// $('#Image1').click(function(){
+//   $('.design').toggle(1000);
+//   $('#view1').toggle(1000);
+// });
+// $('#Image2').click(function(){
+//   $('.development').toggle(1000);
+//   $('#view2').toggle(1000);
+// });
+// $('#Image3').click(function(){
+//   $('.product').toggle(1000);
+//   $('#view3').toggle(1000);
+// });
+// $('#Image1').click(function(){
+//   $('Image1').slideDown('1200').hide('.1000');
+//   $('Image1').show('1000'); 
+// });
+// $('#Image1').click(function(){
+//   $('Image1').slideUp('1200');
+//   $('Image1').slideDown('1000'); 
+// });
+
+// $('#portfolio').hover(function(){
+//     $(this).animate({opacity:"1"});
+//     function:(this){
+//     $(this) animate({opacity:"0"});
+//   });
+// });
