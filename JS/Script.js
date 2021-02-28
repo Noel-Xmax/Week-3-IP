8dbd2e16f8ebf9d20b7d8520d29fb8d0f4c3ded2
Noel Injaga <injaganoel@gmail.com>
2021-02-28 16:03:29 +0300

    added html styling and jquery toggle effect

--- JS/Script.js
+++ JS/Script.js
@@ -13,20 +13,33 @@ $ ('.submit').click(function(event){
 });
 
 });
-$('.design').click(function(){
-  $(".design").toggle();
-  $(".design").toggle();
-})
-$('.development').click(function(){
-  $(".development").toggle();
-  $(".development").toggle();
-})
-$('.product').click(function(){
-  $(".product").toggle();
-  $(".product").toggle();
-})
-$('.Hover').hover(function(){
+// $('.design').click(function(){
+//   $("#design").toggle();
+//   $(".hide").toggle();
+// })
+// $('.development').click(function(){
+//   $("#development").toggle();
+//   $(".hide").toggle();
+// })
+// $('#product').click(function(){
+//   $("#product").toggle();
+//   $(".hide").toggle();
+// })
+$('#Image1').click(function(){
+  $('.design').toggle(1000);
+  $('#view1').toggle(1000);
+});
+$('#Image2').click(function(){
+  $('.development').toggle(1000);
+  $('#view2').toggle(1000);
+});
+$('#Image3').click(function(){
+  $('.product').toggle(1000);
+  $('#view3').toggle(1000);
+});
+$('#portfolio').hover(function(){
     $(this).animate({opacity:"1"});
-  function(this){
-    $(this)animate({opacity:"0"});
+    function:(this){
+    $(this) animate({opacity:"0"});
   });
+});
